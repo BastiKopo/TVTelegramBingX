73cdb2440b8a5d6e04966ab6d1ff493637d2e5d1
BastiKopo <124877759+BastiKopo@users.noreply.github.com>
2025-10-12 07:10:20 +0200

    Delete tvtelegrambingx directory

--- tvtelegrambingx/integrations/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Integration clients used by TVTelegramBingX."""
-
-from .bingx_client import place_order
-
-__all__ = ["place_order"]
--- tvtelegrambingx/webhook/__init__.py
+++
@@ -1,5 +0,0 @@
-"""TradingView webhook server integration for TVTelegramBingX."""
-
-from .server import app, tradingview_webhook
-
-__all__ = ["app", "tradingview_webhook"]
